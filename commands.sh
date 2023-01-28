fa242ba3c04dc86cbb132a75134bae05f1523363
Anivice <lingos@qq.com>
2023-01-28 19:37:41 +0800

    Configuring Mplayer

--- commands.sh
+++ commands.sh
@@ -3,3 +3,13 @@
 # Command for playing audio cd
 mplayer -cdrom-device /dev/sr0 cdda:// -cache 5000
 
+# Configuring Mplayer
+# ou may want to add all options and parameter in your $HOME/.mplayer/config file, so you don’t have to specify every time.
+# Edit ~/.mplayer/config
+
+# default cdrom
+# cdrom="/dev/sr0"
+# cache setting
+# cache=5000
+# set audio driver
+# ao=alsa
